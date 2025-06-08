f00d203cf481041ea2ce2b60a3b20579ffece26c
RisottoMan <lapka.roman@yandex.ru>
2025-06-08 15:26:48 +0500

    add cooldown for abilities

--- SCP999/Interfaces/Ability.cs
+++ SCP999/Interfaces/Ability.cs
@@ -1,4 +1,5 @@
 ﻿using Exiled.API.Features;
+using UnityEngine;
 using UserSettings.ServerSpecific;
 
 namespace Scp999.Interfaces;
@@ -7,6 +8,7 @@ public abstract class Ability : IAbility
     public virtual string Name { get; }
     public virtual string Description { get; }
     public virtual int KeyId { get; }
+    public virtual KeyCode KeyCode { get; }
     public virtual float Cooldown { get; }
     public virtual void Register()
     {
@@ -20,12 +22,23 @@ public abstract class Ability : IAbility
 
     private void OnKeybindActivateAbility(ReferenceHub referenceHub, ServerSpecificSettingBase settingBase)
     {
+        // Check keybind settings
         if (settingBase is not SSKeybindSetting keybindSetting || keybindSetting.SettingId != this.KeyId || !keybindSetting.SyncIsPressed)
             return;
         
+        // Check player
         if (!Player.TryGet(referenceHub, out Player player))
             return;
         
+        // Check cooldown for the ability
+        CooldownComponent cooldown = player.GameObject.GetComponent<CooldownComponent>();
+        if (!cooldown.IsAbilityAvailable(this.Name))
+            return;
+        
+        // Set a cooldown for the ability
+        cooldown.SetCooldownForAbility(this.Name, this.Cooldown);
+        
+        // Activate the ability
         this.ActivateAbility(player);
         Log.Debug($"[Ability] Activating the {this.Name.ToLower()} ability");
     }
